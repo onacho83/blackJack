1e11f76ee7d4577a14f756cd3f27b16f545ece91
OmarB <onacho83@gmail.com>
2021-08-29 20:56:26 -0300

    deck

---
+++ assets/js/juego.js
@@ -0,0 +1,48 @@
+
+let deck =[];
+const tipos = ['C','H','D','S'];
+const especiales = ['J','Q','K','A'];
+// Esta función crea un nuevo deck
+const crearDeck = () => {
+
+    for( let i = 2; i <= 10; i++ ) {
+        for( let tipo of tipos ) {
+            deck.push( i + tipo);
+        }
+    }
+
+    for( let tipo of tipos ) {
+        for( let esp of especiales ) {
+            deck.push( esp + tipo);
+        }
+    }
+    // console.log( deck );
+    deck = _.shuffle( deck );
+    console.log( deck );
+    return deck;
+}
+crearDeck();
+
+const pedirCarta = ()=>{
+
+    if( deck === 0 ){
+        throw 'No hay cartas en el  deck';
+    };
+    const carta = deck.pop();
+
+    console.log(deck);
+    console.log(carta);
+    
+    return carta;
+
+}
+
+pedirCarta();
+
+const valorCarta = (carta)=>{
+return carta;
+
+}
+
+valorCarta('2D');
+
